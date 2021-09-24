35d94c78a6a9f728b8472634eb2c49a3fc50a59f
EngrTeekay <41210739+EngrTeekay@users.noreply.github.com>
2021-09-24 11:05:30 +0100

    http server
    I used three(3) methods to create the http server:
    1. http request/response script
    2. live server from web
    3. localhost server
    The third one is active by default and the first two should work if the comment /* comment*/ is removed.

---
+++ helloworld.js
@@ -0,0 +1,22 @@
+/*creating http server in nodejs
+const http = require('http');
+const host = "localhost";
+const port = 8000;
+//server should respond here
+const requestlistener = function(req, res){
+    res.writeHead(200);
+    res.end("My first server is live!");
+};
+
+//live server here
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log("my server is running on http://${host}:${port}");
+});*/
+
+//local host server runs here on port 8080
+var http = require('htttp');
+http.createServer(function(req, res){
+    res.write("Hellow world, welcome to my http server!");
+    res.end();
+}).listen(8080);
